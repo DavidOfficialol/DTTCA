3e494ff5cb48d3cbdb4bf79ae3b3b7a52f5a8992
David <animalcrossingminecraftdavid@gmail.com>
2026-09-03 18:01:53 +0930

    Initial Commit

---
+++ DTTCA/DTTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  DTTCAApp.swift
+//  DTTCA
+//
+//  Created by Dツvid Official on 3/9/2026.
+//
+
+import SwiftUI
+
+@main
+struct DTTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
